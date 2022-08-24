1eb52d4cd1c31afd7bbb73e95dfdf46071bf27eb
Talha Ahmed Khalid <talha.ahmedm3tech.com.pk>
2022-08-24 15:04:49 +0500

    Setup Completed

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCf5UhsT0ehNZjYeunZP0VEV-GWmqlIheg",
+  authDomain: "hafiztalhakh-practise-project.firebaseapp.com",
+  projectId: "hafiztalhakh-practise-project",
+  storageBucket: "hafiztalhakh-practise-project.appspot.com",
+  messagingSenderId: "163265308639",
+  appId: "1:163265308639:web:fd751f2b6dff3bb70c466a",
+  measurementId: "G-27JL629HKE"
+};
+
+export const app = initializeApp(firebaseConfig);
